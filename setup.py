ceac7afccc6c5fde6717155a64bcd02d781b17f4
Alex G Rice <alex@ricegeo.dev>
2024-03-21 20:25:01 -0600

    Mojo v24.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pytest-mojo",
-    version="24.1.0",
+    version="24.1.1",
     packages=find_packages(),
     entry_points={"pytest11": ["mojo = pytest_mojo.plugin"]},
     install_requires=["pytest"],
